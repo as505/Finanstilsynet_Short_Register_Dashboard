611465965f4dd5206edf6aa6eeb1af48129a66f0
Anders <as505soreide@gmail.com>
2026-03-14 19:12:11 +0100

    styling to position cards

--- frontend/src/routes/instrument/underlyingPosition.svelte
+++ frontend/src/routes/instrument/underlyingPosition.svelte
@@ -28,9 +28,53 @@
 
 </script>
 
-<div>
+<div class="positionContainer">
+	<div class="numberRing"></div>
+	<p class="number">{up_id + 1}</p>
 	<p>{date}</p>
 	<p>{shortPercent}</p>
 	<p>{shares}</p>
 	<p>{positionHolder}</p>
 </div>
+
+<style>
+	
+.positionContainer{
+	background-color: whitesmoke;
+	display: grid;
+	grid-template-columns: 3fr 0.5fr;
+	border-radius: 0.2rem;
+	border-style: solid;
+	border-width: 0.1rem;
+	padding: 1rem;
+	width: 33%;
+	aspect-ratio: 2;
+	box-shadow: 0.5rem 0.2rem 0.05rem grey;
+}
+
+.numberRing{
+	grid-column: 2;
+	grid-row: 1;
+	height: 50%;
+	align-self: center;
+	justify-self: center;
+	aspect-ratio: 1;
+	background-color: orange;
+	border-style: solid;
+	border-radius: 1000rem;
+	border-width: 0.1rem;
+}
+
+.number{
+	grid-column: 2;
+	grid-row: 1;
+	align-self: center;
+	justify-self: center;
+	color: whitesmoke;
+}
+
+p{
+	grid-column: 1;
+}
+
+</style>
